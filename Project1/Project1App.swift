82649937cc0c6835226773c9c7c8c379f6359450
BZV83 <120076188+BZV83@users.noreply.github.com>
2024-10-10 09:05:14 -0600

    Initial Commit

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Brendan Vick on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
