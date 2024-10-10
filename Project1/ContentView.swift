17c3bd129231330a22b26809a07f361841d89ef4
BZV83 <120076188+BZV83@users.noreply.github.com>
2024-10-10 09:20:06 -0600

    Start for my application

--- Project1/ContentView.swift
+++ Project1/ContentView.swift
@@ -7,18 +7,77 @@
 
 import SwiftUI
 
+let topics = [
+    "Basic Greetings and Farewells",
+    "Common Phrases",
+    "Numbers",
+    "Colors",
+    "Family Members",
+    "Food and Drink",
+    "Common Adjectives",
+    "Days of the Week",
+    "Weather Vocab",
+    "Common Verbs"
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(topics, id: \.self) { topic in
+                TopicCell(topic: topic)
+            }
+            .listStyle(.plain)
+            .navigationTitle("SwiftUI Vocabulary")
         }
-        .padding()
     }
 }
 
+struct TopicCell: View {
+    let topic: String
+    
+    var body: some View {
+        HStack {
+            NavigationLink {
+                TopicLessonView(topic: topic)
+            } label: {
+                Text(topic)
+            }
+        }
+    }
+}
+
+struct TopicLessonView: View {
+    let topic: String
+    var body: some View {
+        VStack {
+            Text("Lesson about topic \(topic) goes here.")
+            NavigationLink {
+                QuizScreen()
+            } label: {
+                Text("Take Quiz")
+            }
+        }
+        .navigationTitle("\(topic)")
+    }
+}
+
+struct QuizScreen: View {
+    var body: some View {
+        VStack {
+            Form {
+                Section {
+                    Text("Question")
+                }
+                Section {
+                    Text("A: Answer")
+                    Text("B: Answer")
+                    Text("C: Answer")
+                    Text("D: Answer")
+                }
+            }
+        }
+    }
+}
 #Preview {
     ContentView()
 }
